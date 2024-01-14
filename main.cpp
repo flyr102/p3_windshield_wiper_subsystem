bbd73b9f85d00ba13108bf2c5470e82940e6ba5c
flyr102 <fryerc@union.edu>
2024-01-14 18:39:29 +0000

    project 1 ece 218

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+#include "arm_book_lib.h"
+// main() runs in its own thread in the OS
+int main()
+{
+    DigitalIn driverOccupancy (D2);
+    DigitalIn 
+    while (true) {
+
+    }
+}
+

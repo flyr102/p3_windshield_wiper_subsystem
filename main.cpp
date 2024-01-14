5ad89926cfe377ef6c6db607d6b28d2563f8dce0
flyr102 <fryerc@union.edu>
2024-01-14 19:29:02 +0000

    p1 ece218

--- main.cpp
+++ main.cpp
@@ -1,12 +1,55 @@
+//=====[Libraries]=============================================================
+
 #include "mbed.h"
 #include "arm_book_lib.h"
-// main() runs in its own thread in the OS
-int main()
-{
-    DigitalIn driverOccupancy (D2);
-    DigitalIn 
-    while (true) {
+
+//=====[Defines]===============================================================
+
+//=====[Declaration and initialization of public global objects]===============
+DigitalIn driverSeat (D7);
+DigitalIn driverBelt (D6);
+DigitalIn passengerSeat (D5);
+DigitalIn passengerBelt (D4);
+DigitalIn ignition (D3);
+
+DigitalOut alarmBuzzer (D2);
+DigitalOut ignitionLed (LED1);
+DigitalOut engineLed (LED2);
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void inputsInit();
+void outputsInit();
+
+//=====[Main function, the program entry point after power on or reset]========
+
+int main(){
+    while (true)
+    {
 
     }
 }
+
+//=====[Implementations of public functions]===================================
+
+void inputsInit()
+{
+    driverSeat.mode(PullDown);
+    driverBelt.mode(PullDown);
+    passengerSeat.mode(PullDown);
+    passengerBelt.mode(PullDown);
+    ignition.mode(PullDown);
+}
+
+void outputsInit()
+{
+    alarmBuzzer = OFF;
+    ignitionLed = OFF;
+    engineLed = OFF;
+}
+
 
